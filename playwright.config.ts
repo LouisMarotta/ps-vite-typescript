ef7c7cbcf8564950a8866a1a0e5c071cde002fbe
Louis Marotta <loumb31@gmail.com>
2026-03-04 19:14:17 +0100

    Playwright tests

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const envPath = path.resolve(__dirname, '.env');
+
+dotenv.config({ path: envPath });
+export default defineConfig({
+    use: {
+        baseURL: process.env.BASE_URL
+    }
+})
